603d28798dbbb6189f18a721c56eed08a1350b9a
Marco Mangan <marco.mangan@gmail.com>
2020-07-31 22:50:57 -0300

    Closes #9

--- lib/postal_address_provider.dart
+++ lib/postal_address_provider.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2020, the Postal Code project authors.  Please see
+// the AUTHORS file for details. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
 import 'package:http/http.dart';
 import 'dart:convert';
 import 'postal_address.dart';
@@ -9,7 +13,7 @@ class PostalAddressProvider {
     if (response.statusCode == 200) {
       return PostalAddress.fromJson(json.decode(response.body));
     } else {
-      throw Exception('Failed to load postal address for $code code.');
+      throw Exception('Failed to load owners for $code code.');
     }
   }
 }
--- test/postal_address_provider_test.dart
+++ test/postal_address_provider_test.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2020, the Postal Code project authors.  Please see
+// the AUTHORS file for details. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
 import 'package:postalcode/postal_address_provider.dart';
 import 'package:test/test.dart';
 import 'package:postalcode/postal_address.dart';
